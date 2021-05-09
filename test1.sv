ba0c8801f9cdd592b145eda34c8a32383e057a6c
K-Mozaffari <kamal_mozaffarie@yahoo.com>
2021-05-09 13:09:34 -0400

    Update test1.sv

--- test1.sv
+++ test1.sv
@@ -3,7 +3,8 @@
 program automatic test1(calc2_bus.TB bus);
 import ENV::*;
 import CFG::*;
-  Environment env;
+import   PK::*;
+ 
 task reset;
 bus.reset=1'b1;
 #300ns;
@@ -11,36 +12,42 @@ bus.reset=1'b0;
 #2000ns;
 endtask:reset
 
+
+ class my_trans extends Transaction_Tx;
+ 
+
+	bit [7:0] w_add=100,w_sub=100,w_shl=0,w_shr=0,w_invalid=0,w_nop=0;
+	constraint cmd_dist {cmd dist {4'b0000:=w_nop, 4'b0001:=w_add,4'b0010:=w_sub,4'b0101:=w_shl,4'b0110:=w_shr,[3:4]:/w_invalid,[7:15]:/w_invalid};}
+	constraint c_op1 {op1 inside {[1:32'hfffffffe]};}
+	constraint c_op2 {op2 inside {[1:32'hfffffffe]};}
+	constraint c_tag {req_tag_in inside {[1:3]};}
+
+
+endclass
+
+ Environment env;
+ my_trans newblueprint[1:4];
+
+
   initial begin 
   $display("###################### TEST CASE 1 ######################");
-reset;
+	 reset;
+
 	 env=new(bus );
 	 env.cfg.min_cfg=20 ;
 	 env.cfg.max_cfg=120 ;
-	  env.gen_cfg;
+	 env.gen_cfg;
 	 env.build;
 //---------------------------------------------------------------------------------
   begin 
-    env.gen.build;
-//Test refrence number 1.1
-    for (int i=1;i<5;i++) begin 
-        	env.gen.blueprint[i].constraint_mode(0);
-        	env.gen.blueprint[i].cmd_dist.constraint_mode(1);
-        	env.gen.blueprint[i].c_op1.constraint_mode(1);
-        	env.gen.blueprint[i].c_op2.constraint_mode(1);
-        	env.gen.blueprint[i].c_delay.constraint_mode(1);
-        	env.gen.blueprint[i].req_tag_in.rand_mode(0);
-        	env.gen.blueprint[i].req_tag_in=1;
-        	env.gen.blueprint[i].c_nou_flow.constraint_mode(1);
+  
+  
+ 	foreach (newblueprint[i]) newblueprint[i] =  new;
+	foreach (newblueprint[i]) env.gen.blueprint[i]=newblueprint[i];
 
-			env.gen.blueprint[i].w_delay_insert=0;
-			env.gen.blueprint[i].w_add=100;
-    end
   end 
-  env.buffer_size=1;
-//--------------------------------------------------------------------------------- 
+  //--------------------------------------------------------------------------------- 
     env.run;
-
     env.wrap_up;
  end
 endprogram
