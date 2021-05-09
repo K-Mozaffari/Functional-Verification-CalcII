4b933dacd9fb2af05119a6fcf057f0da44c06cb0
K-Mozaffari <kamal_mozaffarie@yahoo.com>
2021-05-09 13:51:53 -0400

    Update test3.sv

--- test3.sv
+++ test3.sv
@@ -3,49 +3,56 @@
 program automatic test3(calc2_bus.TB bus);
 import ENV::*;
 import CFG::*;
-  Environment env;
+  import   PK::*;
 task reset;
 bus.reset=1'b1;
 #300ns;
 bus.reset=1'b0;
 #2000ns;
 endtask:reset
+ class my_trans extends Transaction_Tx;
+ 
+	bit dif;
+	bit [7:0] w_add=0,w_sub=0,w_shl=0,w_shr=0,w_invalid=0,w_nop=0;
+	constraint cmd_dist {cmd dist {4'b0000:=w_nop, 4'b0001:=w_add,4'b0010:=w_sub,4'b0101:=w_shl,4'b0110:=w_shr,[3:4]:/w_invalid,[7:15]:/w_invalid};}
+	constraint c_op1 {op1 inside {[1:32'hfffffffe]};}
+	constraint c_op2 {op2 inside {[1:32'hfffffffe]};}
+	constraint c_tag {req_tag_in inside {[1:3]};}
+	
+	constraint c_ou_flow {((cmd==4'b0001)->(op1+op2==0));( (cmd==4'b0010&&dif==0)->(op1<op2));(cmd==4'b0010&&dif==1)->(op2-op1==1);}
+	
+
+
+endclass
+
+ Environment env;
+ my_trans newblueprint[1:4];
+
 
   initial begin 
   $display("###################### TEST CASE 3 ######################");
-reset;
+	 reset;
+
 	 env=new(bus );
 	 env.cfg.min_cfg=1 ;
 	 env.cfg.max_cfg=20 ;
-	  env.gen_cfg;
+	 env.gen_cfg;
 	 env.build;
 //---------------------------------------------------------------------------------
   begin 
-    env.gen.build;
-//Test refrence number 3
-    for (int i=1;i<5;i++) begin 
-		env.gen.blueprint[i].constraint_mode(0);
-		env.gen.blueprint[i].cmd_dist.constraint_mode(1);
-		env.gen.blueprint[i].c_op1.constraint_mode(1);
-		env.gen.blueprint[i].c_op2.constraint_mode(1);
-		env.gen.blueprint[i].c_delay.constraint_mode(1);
-		env.gen.blueprint[i].req_tag_in.rand_mode(0);
-		env.gen.blueprint[i].req_tag_in=1;
-		env.gen.blueprint[i].c_ou_flow.constraint_mode(1);
-
-		env.gen.blueprint[i].w_delay_insert=0;
-
-		env.gen.blueprint[i].sum=0;
-		env.gen.blueprint[i].w_add=50;
-		env.gen.blueprint[i].w_sub=50;
-
-    end
+  
+ 
+ 	foreach (newblueprint[i]) newblueprint[i] =  new;
+ 	foreach (newblueprint[i]) 
+		begin 
+			newblueprint[i].w_add=50;
+			newblueprint[i].w_sub=50;
+		end;
+	foreach (newblueprint[i]) env.gen.blueprint[i]=newblueprint[i];
 
   end 
-  env.buffer_size=1;
-//--------------------------------------------------------------------------------- 
+  //--------------------------------------------------------------------------------- 
     env.run;
-
     env.wrap_up;
  end
 endprogram
